ab4614d465fc96d2e0c66cd70c467e38d291f3df
dortcom <dortcom@dortcoms-MacBook-Air.local>
2022-05-01 23:36:21 +0000

    my First Commit

---
+++ practice.js
@@ -0,0 +1,85 @@
+console.log('foster')
+        console.log(45)
+        console.log(true, false)
+
+
+   
+        //data types
+var name = 'nnegred'
+console.log (name)
+
+name = 'jkhbhj'
+
+console.log (name)
+
+let him = 'fred'
+
+console.log (him)
+
+him = 'bernard'
+console.log (him)
+
+const her = 'ama'
+
+console.log (her)
+
+
+
+var firstname = 'frimpong'
+
+console.log (firstname)
+
+var lastname =("ashiama");
+
+document.write (lastname);
+
+
+var equation = 3 * 4;
+console.log (equation);
+
+equation = 10 % 2;
+
+console.log (equation)
+
+var me = 7 + 10;
+
+console.log (++me)
+
+var you = 6 + 2;
+console.log (you++);
+
+console.log (you)
+
+
+console.log ( 5 > 5);
+
+var them = 10 < 2;
+
+console.log (them);
+
+    
+
+var it = 2 <10 || 4< 3;
+console.log ( it);
+
+var sandi = ('her first name\n her last name')
+console.log (sandi)
+
+console.log(38%5);
+
+var a =0; b=1;
+console.log(a)
+
+a=++b;
+console.log(a)
+a=b++
+console.log(a)
+
+var isanadult = (age < 18)? "too old" : "too young";
+var age=20;
+console.log(isanadult);
+
+var strings = ('my first name')
+var strings2 = (' my last name')
+
+console.log (strings +  strings2)
