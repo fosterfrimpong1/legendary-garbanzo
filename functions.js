51557c5627e08547265048bba928ffd7f3935562
dortcom <dortcom@dortcoms-MacBook-Air.local>
2022-05-29 05:40:10 +0000

    my First Commit

--- functions.js
+++ functions.js
@@ -49,3 +49,130 @@ return rows * columns;
 }
 const numOfMonitors =monitorCount(5,4);
 console.log(numOfMonitors);
+
+
+//Helper Functions!
+
+function multiplyByNineFifths(number) {
+  return number * (9/5);
+};
+ 
+function getFahrenheit(celsius) {
+  return multiplyByNineFifths(celsius) + 32;
+};
+ 
+getFahrenheit(15); // Returns 59
+
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+function costOfMonitors (rows, columns){
+  return monitorCount(rows, columns) * 200
+}
+
+const totalCost = costOfMonitors(5, 4)
+
+console.log(totalCost);
+
+//Function Expressions!
+const plantNeedsWater = function(day){
+  if (day==='Wednesday'){
+    return true;
+  } else{
+    return false;
+  }
+};
+plantNeedsWater('Tuesday')
+console.log(plantNeedsWater('Tuesday'));
+
+//Arrow Functions!
+const rectangleArea = (width, height) => {
+  let area = width * height;
+  return area;
+};
+
+const plantNeedWater = (day) => {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+plantNeedWater('Wednesday')
+console.log(plantNeedWater('Wednesday'));
+
+//Concise Body Arrow Functions!
+//if a function looks like this//
+const greaterThanFive = (num) => {
+  return num > 5 ? true : false;
+};
+
+//to make a concise body, we would remove the parenthesis!
+const greaterThanFive = num =>  num > 5 ? true : false;
+
+
+const plantNeedsWater = day => 
+ day === 'Wednesday' ? true : false;
+
+
+ //Block and Scope//
+
+ const city = 'New York City';
+function logCitySkyline (){
+ 
+  let skyscraper = 'Empire State Building'
+   return 'The stars over the ' + skyscraper + ' in ' + city;
+};
+console.log(logCitySkyline());
+
+//Global Scope//
+
+let satellite = 'The Moon'
+let galaxy = 'The Milky Way'
+let stars = 'North Star'
+
+function callMyNightSky(){
+
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+
+console.log(callMyNightSky());
+
+//Block Scope//
+function logVisibleLightWaves(){
+
+  const lightWaves = 'Moonlight'
+  console.log(lightWaves)
+};
+console.log(logVisibleLightWaves())
+console.log(lightWaves)
+
+const logSkyColor = () => {
+  let color = 'blue'; 
+  console.log(color); // blue 
+};
+ 
+logSkyColor(); // blue 
+console.log(color); // ReferenceError
+
+//Practice good scoping//
+const logVisibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+	let region = 'The Arctic';
+  // Add if statement here:
+  if (region === 'The Arctic'){
+    let lightWaves = 'Northern Lights'
+    console.log(lightWaves)
+  }
+  
+  console.log(lightWaves);
+};
+
+logVisibleLightWaves();
+
+//Arrays//
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+
+
+
---
+++ virtual.js
@@ -0,0 +1,20 @@
+
+
+
+const hood = 'Akweteyman'
+
+function logLarghetto (){
+ var structure = 'The ghetto layout is not well arranged in '
+ return structure + hood
+ };
+ console.log(logLarghetto());
+
+
+ var school = 'codetrain';
+
+ function logThecourse(){
+ let program = 'has a nice program'
+ return school + '' + program
+ };
+
+ console.log(logThecourse());
