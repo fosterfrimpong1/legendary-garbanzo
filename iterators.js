c637133c0098981135e270030238333831c8508a
dortcom <dortcom@dortcoms-MacBook-Air.local>
2022-06-06 19:57:29 +0000

    my First Commit

--- iterators.js
+++ iterators.js
@@ -3,3 +3,116 @@ const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 
 // Iterate over fruits below
 fruits.forEach(fruit => console.log(`I want to eat a ${fruit}.`))
+
+//The .map() Method//
+
+const numbers = [1, 2, 3, 4, 5]; 
+ 
+const bigNumbers = numbers.map(number => {
+  return number * 10;
+});
+
+console.log(numbers); // Output: [1, 2, 3, 4, 5]
+console.log(bigNumbers); // Output: [10, 20, 30, 40, 50]
+
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+const secretMessage = animals.map(animal => animal[0]);
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(num => num/100);
+
+console.log(smallNumbers)
+
+
+//The .filter() Method//
+const words = ['chair', 'music', 'pillow', 'brick', 'pen', 'door']; 
+ 
+const shortWords = words.filter(word => {
+  return word.length < 6;
+});
+
+console.log(words); // Output: ['chair', 'music', 'pillow', 'brick', 'pen', 'door']; 
+console.log(shortWords); // Output: ['chair', 'music', 'brick', 'pen', 'door']
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter(num => {
+  return num < 250
+});
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+
+// Call .filter() on favoriteWords below
+
+const longFavoriteWords = favoriteWords.filter(word =>{
+  return word.length > 7
+})
+
+//The .findIndex() Method//
+const jumbledNums = [123, 25, 78, 5, 9]; 
+ 
+const lessThanTen = jumbledNums.findIndex(num => {
+  return num < 10;
+});
+
+console.log(lessThanTen); // Output: 3 
+console.log(jumbledNums[3]); // Output: 5
+
+const greaterThan1000 = jumbledNums.findIndex(num => {
+  return num > 1000;
+});
+ 
+console.log(greaterThan1000); // Output: -1
+
+
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+
+
+const foundAnimal = animals.findIndex(animal => {
+  return animal === ('elephant')
+});
+console.log(foundAnimal)
+
+const startsWithS = animals.findIndex( animal => {
+
+  return animal[0] === 's' ? true : false;
+})
+console.log(startsWithS);
+
+//The .reduce() Method//
+const numbers = [1, 2, 4, 10];
+ 
+const summedNums = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue
+})
+ 
+console.log(summedNums) // Output: 17
+
+
+const numbers = [1, 2, 4, 10];
+ 
+const summedNums = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue
+}, 100)  // <- Second argument for .reduce()
+ 
+console.log(summedNums); // Output: 117
+
+
+
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue;
+}, 10);
+
+console.log(newSum);
--- object.js
+++ object.js
@@ -15,3 +15,71 @@ function person(name, age) {
 function bornYear() {
   return 2016 - this.age;
 }
+
+// An object literal with two key-value pairs
+let spaceship = {
+  'Fuel Type': 'diesel',
+  color: 'silver'
+};
+
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+// Write your code below
+var crewCount= spaceship.numCrew
+var planetArray = spaceship.flightPath
+
+//Bracket Notation//
+let spaceship = {
+  'Fuel Type': 'Turbo Fuel',
+  'Active Duty': true,
+  homePlanet: 'Earth',
+  numCrew: 5
+};
+spaceship['Active Duty'];   // Returns true
+spaceship['Fuel Type'];   // Returns  'Turbo Fuel'
+spaceship['numCrew'];   // Returns 5
+spaceship['!!!!!!!!!!!!!!!'];   // Returns undefined
+
+
+let returnAnyProp = (objectName, propName) => objectName[propName];
+ 
+returnAnyProp(spaceship, 'homePlanet'); // Returns 'Earth'
+
+//Property Assignment//
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+// Write your code below
+spaceship.color = 'glorious gold';
+
+spaceship.numEngines=9
+delete spaceship['Secret Mission'];
+
+
+//Methods//
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+// Write your code below
+
+let alienShip = {
+  retreat() {
+    console.log(retreatMessage)
+  },
+  takeOff() {
+    console.log('Spim... Borp... Glix... Blastoff!')
+  }
+};
+
+alienShip.retreat();
+
+alienShip.takeOff();
